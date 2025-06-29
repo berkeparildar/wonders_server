a427f596e6e3a4c03320398d0664db3fba001550
Berke Parıldar <94047726+berkeparildar@users.noreply.github.com>
2025-06-29 22:42:23 +0300

    Initial commit

---
+++ src/main/java/com/bprldr/wonders_server/WondersServerApplication.java
@@ -0,0 +1,13 @@
+package com.bprldr.wonders_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WondersServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WondersServerApplication.class, args);
+    }
+
+}
